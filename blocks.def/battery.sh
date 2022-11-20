a5d29395746c4acc2ee7a7327ec53db4600ab878
bzenz <bzenz@t-online.de>
2022-11-20 12:03:24 +0100

    Low battery notification + sound

--- blocks.def/battery.sh
+++ blocks.def/battery.sh
@@ -2,3 +2,9 @@
 ICON=""
 read -r capacity </sys/class/power_supply/BAT0/capacity
 printf "$ICON %s%%" "$capacity"
+if [ "$capacity" -lt "16" ]
+then
+  notify-send "Battery low"
+  mpv --no-video ~/Music/Sounds/warning.mp3
+fi
+
