58d42652770828a03fca5ceba9068f9b1eae4d7d
bzenz <benito@benitozenz.de>
2024-07-26 18:53:42 +0200

    Add different battery icons

--- blocks.def/battery.sh
+++ blocks.def/battery.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
-ICON=""
+CHARGE_ICON=""
+DISCHARGE_ICON0=""
+DISCHARGE_ICON1=""
+DISCHARGE_ICON2=""
+DISCHARGE_ICON3=""
+DISCHARGE_ICON4=""
+
 read -r capacity </sys/class/power_supply/BAT0/capacity
-printf "$ICON %s%%" "$capacity"
-if [ "$capacity" -lt "16" ]
-then
+read -r powerstatus </sys/class/power_supply/BAT0/status
+
+if [ "$powerstatus" == "Charging" ]; then
+  ICON=$CHARGE_ICON
+elif [ "$capacity" -lt "20" ]; then
+  ICON=$DISCHARGE_ICON0
+
+  # Send warning
   notify-send "Battery low"
   mpv --no-video ~/Music/Sounds/warning.mp3
+elif [ "$capacity" -lt "40" ]; then
+  ICON=$DISCHARGE_ICON1
+elif [ "$capacity" -lt "60" ]; then
+  ICON=$DISCHARGE_ICON2
+elif [ "$capacity" -lt "80" ]; then
+  ICON=$DISCHARGE_ICON3
+else
+  ICON=$DISCHARGE_ICON4
 fi
+
+# Print output
+printf "$ICON %s%%" "$capacity"
 
