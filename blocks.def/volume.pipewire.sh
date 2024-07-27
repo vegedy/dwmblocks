3107eaf8e6497257ff16bd616658a76013c7876d
bzenz <benito@benitozenz.de>
2024-07-27 19:04:36 +0200

    Improve volume

--- blocks.def/volume.pipewire.sh
+++ blocks.def/volume.pipewire.sh
@@ -1,44 +1,25 @@
-#!/bin/sh
-# (for pipewire users)
-# This script parses the output of `pactl list sinks' to find volume and mute
-# status of the default audio sink and whether headphones are plugged in or not
-# Also see ../daemons/pulse_daemon.sh
-sink="$(pactl info | awk '$1 == "Default" && $2 == "Sink:" {print $3}')"
-[ -n "$sink" ] || exit
-pactl list sinks | awk -v sink="$sink" '
-    BEGIN {
-        ICONsn = "\x0c\x0b" # headphone unplugged, not muted
-        ICONsm = "\x0d\x0b" # headphone unplugged, muted
-        ICONhn = "\x0c\x0b" # headphone plugged in, not muted
-        ICONhm = "\x0d\x0b" # headphone plugged in, muted
-    }
-    f {
-        if ($1 == "Mute:" && $2 == "yes") {
-            m = 1
-        } else if ($1 == "Volume:") {
-            if ($3 == $10) {
-                vb = $5
-            } else {
-                vl = $5
-                vr = $12
-            }
-        } else if ($1 == "Active" && $2 == "Port:") {
-            if (tolower($3) ~ /headphone/)
-                h = 1
-            exit
-        }
-        next
-    }
-    $1 == "Name:" && $2 == sink {
-        f = 1
-    }
-    END {
-        if (f) {
-            printf "%s ", h ? (m ? ICONhm : ICONhn) : (m ? ICONsm : ICONsn)
-            if (vb)
-                print vb
-            else
-                printf "L%s R%s\n", vl, vr
-        }
-    }
-'
+#!/bin/bash
+#!/bin/bash
+
+# Ermitteln des Standard-Sinks
+sink=$(pactl info | awk '$1 == "Default" && $2 == "Sink:" {print $3}')
+
+# Lautstärke extrahieren
+volume=$(pactl list sinks | grep "Name: $sink" -A 7 | awk -F/ '/Volume:/ {gsub("%", ""); print $2}' | xargs)
+
+# Mute-Status extrahieren
+mute=$(pactl list sinks | grep "Name: $sink" -A 7 | awk -F: '/Mute:/ {print $2}' | xargs)
+
+# Ausgabe basierend auf Mute-Status und Lautstärke
+if [ "$mute" == "yes" ]; then
+    echo "🔇 Mute"
+else
+    if [ "$volume" -ge 66 ]; then
+        echo "🔊 $volume%"
+    elif [ "$volume" -ge 33 ]; then
+        echo "🔉 $volume%"
+    else
+        echo "🔈 $volume%"
+    fi
+fi
+
--- blocks.def/volume_button.sh
+++ blocks.def/volume_button.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 case "$1" in
-    1) pactl set-sink-mute @DEFAULT_SINK@ toggle ;;
+    1) pactl set-sink-mute @DEFAULT_SINK@ toggle; sigdwmblocks 2 ;;
     3) pactl set-sink-volume @DEFAULT_SINK@ 50% ;;
 esac
